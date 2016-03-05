0f737ca8518ae97dd2d372dbc204c795a9deed87
selichter <lichter.sarahe@gmail.com>
2016-03-05 15:08:06 -0600

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,23 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it ('should return femail contacts only,' function() {
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should select people from Manchester' function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(8);
+
+    });
+
 });
