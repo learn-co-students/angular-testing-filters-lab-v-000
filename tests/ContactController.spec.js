b799566094cd9e4c783a5d35b1cf1b54cc1c6876
John Anthony Rivera <askinganthony@gmail.com>
2016-03-11 17:31:36 -0800

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,23 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter the results correctly if female is searched', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+        $scope.vm.search = 'female';
+        $scope.vm.changeFilter();
+        expect($scope.vm.filteredList.length).toEqual(71);
+        
+    });
+
+    it('should filter the results correctly when location city is seached', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'Manchester';
+        $scope.vm.changeFilter();
+        expect($scope.vm.filteredList.length).toEqual(8);
+        
+    });
+
 });
