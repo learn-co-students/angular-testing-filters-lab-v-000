57edb83b320efe206f672bb62ce86302848d647d
Justin Thomas <jthomas0209@gmail.com>
2016-03-28 11:33:32 -0400

    Done

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,20 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return all female contacts', function(){
+      var $scope = {};
+      $controller('ContactController as vm', {$scope:$scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it ('should return all contacts from Manchester', function(){
+      var $scope = {};
+      $controller('ContactController as vm', {$scope:$scope});
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+    });
+
 });
