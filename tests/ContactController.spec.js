e9b0cfe4ebfe7aa77c1ad906f2b394258cea15c0
Adam King <adam.king0126@gmail.com>
2016-04-05 13:37:55 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,25 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should find all 71 females', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should find everyone in Manchester', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'Manchester';
+
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(8);
+    });
+
+
 });
