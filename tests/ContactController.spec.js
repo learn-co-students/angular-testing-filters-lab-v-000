e48598ccd7899d22c9a5470a0a93a965771c56a9
zachnewburgh <zach.newburgh@gmail.com>
2016-04-16 11:36:08 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,21 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter female results correctly', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it('should filter Manchester results correctly', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'Manchester'
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+    })
 });
