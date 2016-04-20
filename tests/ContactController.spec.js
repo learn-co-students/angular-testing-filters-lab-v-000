958fe092eb6e3ca3f17dbfa30ce2891a02b561ce
Marco Tsui <marcotsui2003@yahoo.com>
2016-04-20 10:19:14 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,22 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it("can filter gender", function() {
+      $scope ={};
+      $controller("ContactController as cntrl", {$scope: $scope});
+
+      $scope.cntrl.search =  'female';
+      $scope.cntrl.changeFilter();
+      expect($scope.cntrl.filteredList.length).toEqual(71);
+    });
+
+    it("can select people from Manchester", function() {
+      $scope ={};
+      $controller("ContactController as cntrl", {$scope: $scope});
+
+      $scope.cntrl.search = "Manchester";
+      $scope.cntrl.changeFilter();
+      expect($scope.cntrl.filteredList.length).toEqual(8);
+    })
+
 });
