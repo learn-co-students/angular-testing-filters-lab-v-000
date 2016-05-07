e87cd2b755853573b22302edcfc82a3feaea0689
Brian <useyourbrian@gmail.com>
2016-05-07 08:25:41 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,4 +1,4 @@
-describe('ContactController', function () {
+escribe('ContactController', function () {
     var $controller;
 
     beforeEach(module('app'));
@@ -6,5 +6,26 @@ describe('ContactController', function () {
     beforeEach(inject(function (_$controller_) {
         $controller = _$controller_;
     }));
+
+
+    it('should filter by gender', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'female';
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should filter by location', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'manchester';
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toBe(8);
+    });
 
 });
