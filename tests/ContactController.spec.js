9ab688130754449e5a65d080c735addf87247067
authorbeard <msnolan@gmail.com>
2016-05-20 11:19:08 -0400

    all tests written/passing

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,18 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter the results correctly', function(){
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope})
+      
+      $scope.vm.search = "female"
+      $scope.vm.changeFilter()
+      expect($scope.vm.filteredList.length).toEqual(71)
+
+      $scope.vm.search="Manchester"
+      $scope.vm.changeFilter()
+      expect($scope.vm.filteredList.length).toEqual(8)
+
+    })
+
 });
