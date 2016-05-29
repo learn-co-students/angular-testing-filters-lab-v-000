8aba53e4ebbbee1215ec847ef289144b40195da6
evanscloud <dijkstrascloud@gmail.com>
2016-05-29 00:15:11 -0400

    Modify tests to work with corresponding filters in controller

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,21 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter all the females correctly'), function(){
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = "female";
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(71);
+
+      it('should filter all the people from Manchester correctly'), function(){
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = "Manchester";
+        $scope.vm.changeFilter();
+        expect($scope.vm.filteredList.length).toEqual(8);
+    }
+
 });
