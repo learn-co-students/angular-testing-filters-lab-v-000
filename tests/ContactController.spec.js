811a7cc9950a091a756e4f49932ddc66d830a896
seanliu93 <sean93liu@gmail.com>
2016-06-16 20:35:40 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,20 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return all female contacts', function() {
+      var $scope = {}
+      $controller('ContactController as vm',{$scope: $scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('selects people from different locations', function() {
+      var $scope = {};      
+      $controller('ContactController as vm', {$scope: $scope});      
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toBe(8);
+    });
+
 });
