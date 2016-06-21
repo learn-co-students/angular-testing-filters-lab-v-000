cbb9f09d8015abb86a1a2656a155e5bf3173df6a
Dakota Martinez <dakotaleemusic@gmail.com>
2016-06-21 06:46:08 +0000

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,10 +1,34 @@
 describe('ContactController', function () {
-    var $controller;
+  var $controller;
 
-    beforeEach(module('app'));
+  beforeEach(module('app'));
 
-    beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
-    }));
+  beforeEach(inject(function (_$controller_) {
+    $controller = _$controller_;
+  }));
+  
+  it('should properly filter all of the contacts in the list by gender', function(){
+    var $scope = {};
+    
+    $controller('ContactController as vm', {$scope: $scope});
+    
+    $scope.vm.search = "female";
+    
+    $scope.vm.changeFilter();
+    
+    expect($scope.vm.filteredList.length).toEqual(71);
+  });
+  
+  it('should properly filter all of the contacts in the list by geographical location', function(){
+    var $scope = {};
+    
+    $controller('ContactController as vm', {$scope: $scope});
+    
+    $scope.vm.search = "Manchester";
+    
+    $scope.vm.changeFilter();
+    
+    expect($scope.vm.filteredList.length).toEqual(8);
+  });
 
 });
