b6f2275cde174b252c9387dfda8e45bfd76d259f
katherinethai <kthai1618@gmail.com>
2016-06-21 12:17:19 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -4,7 +4,29 @@ describe('ContactController', function () {
     beforeEach(module('app'));
 
     beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
+        $controller = _$controller_;  
     }));
+
+    it('should filter list and return only women', function () {
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope}),
+
+      $scope.vm.search = 'female';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it('should filter list and return only people living in Manchester', function () {
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope}),
+
+      $scope.vm.search = 'Manchester';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+    });
 
 });
