51cd4a71c9df6321959f7c340356850e9368ec70
cassaram09 <cassaram09@gmail.com>
2016-08-04 22:14:41 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,23 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    var $scope={};
+
+    it('should return all females', function () {
+      
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      angular.forEach($scope.vm.filteredList, function(person, index){
+        expect(person.gender).toEqual('female');
+      });
+    });
+
+    it('should count people from Manchester', function () {
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+    });
+
 });
