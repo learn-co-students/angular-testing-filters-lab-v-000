d6cbe2576dd2b305a2f2d388dd31c9b910ed4d7a
nscarlyon <natasha.carlyon@gmail.com>
2016-08-29 14:10:01 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,23 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return all the results that are female', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter()
+
+    expect($scope.vm.filteredList.length).toEqual(71);
+    })
+
+    it('should select everybody from Manchester', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = "Manchester";
+      $scope.vm.changeFilter()
+
+      expect($scope.vm.filteredList.length).toEqual(8)
+
+    })
+
 });
