bddec3dea469dfecdf270aba89e2a48417503c29
hudbuz <hzb2@cornell.edu>
2016-09-07 11:36:26 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,25 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return a list of all females', function() {
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+
+    it('should return everyone from one Geographic location', function() {
+      var $scope = {}
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+    });
+
+
+
 });
