ee2e747ccd9dddb1d34e1a01e6ac8b6fb5684793
Jason <jasonkwong11@gmail.com>
2016-09-12 04:27:56 -0400

    done

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,29 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return all the contacts who are female', function(){
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      //$scope.vm holds all of our values
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it('should select people from different geographical locations', function(){
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = "Manchester";
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+    })
+
+
 });
+
+
+//Write a test to return all the contacts who are female 
+//(hint: there are 71 females).
+
+//Write another test to select people from different geographical locations 
+//(hint: there are 8 from Manchester).
