cc0ab36e3678ef921d46e439166748ea6d2c0eda
Saturn226 <serenehopes@gmail.com>
2016-10-02 20:56:11 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,23 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return female contacts', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        $scope.vm.search = 'female';
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toBe(71);
+    });
+
+    it('should return people from other countries', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'manchester';
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toBe(8);
+    });
 });
