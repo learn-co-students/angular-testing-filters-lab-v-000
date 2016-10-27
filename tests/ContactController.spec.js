c0819c83527cafb5df2f98ad11adf19a667c6438
rgylling <mikegylling@yahoo.com>
2016-10-27 10:45:47 -0700

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,31 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter contacts by gender', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female'
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(71);
+
+    });
+
+    it('should filter contacts by location', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'Manchester'
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+
+    });
+
+
+
+
 });
