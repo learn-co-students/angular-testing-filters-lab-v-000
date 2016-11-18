4483f963e0e5c95c23aca75a75efa27c5e82401c
github username <github email address>
2016-11-18 12:56:42 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,21 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter the results correctly', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+
+        expect($scope.vm.filteredList.length).toEqual(71);
+      })
+
+    it('should re-filter the results correctly when changing search term', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+  });
+
+
 });
