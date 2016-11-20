9903bf26d52fd4f92f43bb430d1fa0941be8f912
kade042 <dereje88@gmail.com>
2016-11-20 00:16:58 -0700

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,26 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter contacts by gender (e.g female)', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+        $scope.vm.search = 'female';
+
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it('should filter from different geographical locations (e.g Manchester)', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+        $scope.vm.search = 'Manchester';
+
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toEqual(8);
+    });
+
+
+
 });
