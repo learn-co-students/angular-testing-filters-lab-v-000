225621ca8a4b7a1ce983137ee938070361bda822
Christian Whitesides <whitesidescr@gmail.com>
2016-12-15 16:08:33 -0700

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,26 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return the number of females on the list', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it('should return the number of contacts on the list with manchester as their city', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'manchester';
+
+      $scope.vm.changeFilter();
+
+      expect($scope.vm.filteredList.length).toEqual(8);
+    });
+
 });
