71177b3a8f528a0da7c78c52a886894a7b3db71b
Ann Lee <aplee29@users.noreply.github.com>
2017-01-21 07:28:07 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,10 +1,32 @@
 describe('ContactController', function () {
-    var $controller;
+  var $controller;
 
-    beforeEach(module('app'));
+  beforeEach(module('app'));
 
-    beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
-    }));
+  beforeEach(inject(function (_$controller_) {
+    $controller = _$controller_;
+  }));
 
+  it('should filter all females', function () {
+    var $scope = {};
+    $controller('ContactController as vm', {$scope: $scope});
+
+    $scope.vm.search = 'female';
+
+    $scope.vm.changeFilter();
+
+    // expect($scope.vm.filteredList.length).toEqual(71);
+    expect($scope.vm.filteredList.length).toBe(71);
+  });
+
+  it('should filter by geographical location', function () {
+    var $scope = {};
+    $controller('ContactController as vm', {$scope: $scope});
+ 
+    $scope.vm.search = 'manchester';
+ 
+    $scope.vm.changeFilter();
+ 
+    expect($scope.vm.filteredList.length).toEqual(8);
+  });
 });
