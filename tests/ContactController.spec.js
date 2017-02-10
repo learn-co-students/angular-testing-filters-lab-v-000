1d732070e7a42f70eb075fedc447a67f805232dc
hilaryml <hilyttle@gmail.com>
2017-02-10 13:24:30 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,20 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should return all the contacts that are female', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(71);
+    })
+
+    it('should return all people from a geographic location', function () {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+    })
+
 });
