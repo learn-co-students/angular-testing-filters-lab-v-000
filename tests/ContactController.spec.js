63f6950157e7ae100198fbd326691d64410b9882
Clare <clare.e.horton@gmail.com>
2017-02-12 01:51:04 -0600

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,28 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('can search by gender', function() {
+      var $scope = {};
+
+      $controller('ContactController as vm', {$scope: $scope});
+
+      $scope.vm.search = 'female';
+      $scope.vm.changeFilter();
+      var men = [];
+      var women = $scope.vm.filteredList.map(function(x) {
+        if ( x["gender"] === "male" ) {
+          men.push(x);
+        }
+      })
+      expect(men.length).toEqual(0);
+    });
+
+    it('can search by location', function() {
+      var $scope = {};
+      $controller('ContactController as vm', {$scope: $scope});
+      $scope.vm.search = 'Manchester';
+      $scope.vm.changeFilter();
+      expect($scope.vm.filteredList.length).toEqual(8);
+    })
+
 });
