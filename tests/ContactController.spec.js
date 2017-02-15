b61080fc737c4cf8b804930de63827093c883ace
Bill <storywr@gmail.com>
2017-02-15 14:32:27 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,10 +1,31 @@
 describe('ContactController', function () {
-    var $controller;
+  var $controller;
 
-    beforeEach(module('app'));
+     beforeEach(module('app'));
 
-    beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
-    }));
+     beforeEach(inject(function (_$controller_) {
+         $controller = _$controller_;
+     }));
+
+
+     it('should filter by gender', function () {
+         var $scope = {};
+         $controller('ContactController as vm', {$scope: $scope});
+
+         $scope.vm.search = 'female';
+         $scope.vm.changeFilter();
+
+         expect($scope.vm.filteredList.length).toBe(71);
+     });
+
+     it('should filter by location', function () {
+         var $scope = {};
+         $controller('ContactController as vm', {$scope: $scope});
+
+         $scope.vm.search = 'manchester';
+         $scope.vm.changeFilter();
+
+         expect($scope.vm.filteredList.length).toBe(8);
+     });
 
 });
